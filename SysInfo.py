7a3eaf37f94d09edcb3d2ff67f350564bea2cb84
Yash Surpaithankar <surpaithankary19@gmail.com>
2026-06-24 09:54:34 +0530

    Initial Commit

---
+++ SysInfo.py
@@ -0,0 +1,8 @@
+import platform
+
+print("System Information")
+print("------------------")
+print("OS:", platform.system())
+print("Version:", platform.version())
+print("Machine:", platform.machine())
+print("Developed by Whitelabs24.")
